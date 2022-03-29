d5df9fca10795a3cae4443b0a12661602176886b
takashi-ogaya <takashi.ogaya@gmail.com>
2022-03-29 18:59:25 +0900

    レビュー後修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :nickname, presence: true
   validates :birthday, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
@@ -17,6 +17,6 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
 
-  has_many :items
-  has_many :purchases
+  # has_many :items
+  # has_many :purchases
 end
