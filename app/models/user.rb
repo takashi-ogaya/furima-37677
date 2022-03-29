e1e712a76bf4d8d45f19c42dfe440ad7f3649003
takashi-ogaya <takashi.ogaya@gmail.com>
2022-03-29 17:05:09 +0900

    テストコード完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,5 +19,7 @@ class User < ApplicationRecord
   
   has_many :items
   has_many :purchases
+ 
+
 end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password { "1a1a1a" }
+    password_confirmation { "1a1a1a" }
+    last_name {Gimei.last.kanji}
+    first_name {Gimei.first.kanji}
+    last_name_kana {Gimei.last.katakana}
+    first_name_kana {Gimei.first.katakana}
+    birthday { "1979-06-08" }
   end
 end
