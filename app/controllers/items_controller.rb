95c3ca56e9ea6fd5b874e28a5788c0280e54ea5a
takashi-ogaya <takashi.ogaya@gmail.com>
2022-03-29 14:44:51 +0900

    Rspec 導入；テストコード記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,61 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :sold_edit, only: :edit
   def index
     # @items = Item.all.order(created_at: :desc)
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  def edit
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+      if @item.update(item_params)
+        redirect_to item_path(params[:id])
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to action: :index
+  end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :category_id, :situation_id, :postage_id, :city_id, :shipping_date_id, :explanation, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def sold_edit
+    if @item.purchase.present?
+      redirect_to root_path
+    end
+  end
+
 end
