86d2dbdc115092230465f89af33111b7c8c510a6
takashi-ogaya <takashi.ogaya@gmail.com>
2022-04-02 19:27:13 +0900

    edit html erb 編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,8 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    # redirect_to action: :index unless current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
   end
 
   def update
