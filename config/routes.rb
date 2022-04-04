a5ba7b81b0b4629d37c7b1c6b89c1b49621222c2
takashi-ogaya <takashi.ogaya@gmail.com>
2022-04-04 21:58:31 +0900

    レビュー後１

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-    resources :purchases
+    resources :purchases, only: [:create, :index]
   end
 end
